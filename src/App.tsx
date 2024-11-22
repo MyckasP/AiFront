a31daaffa5b29b6a0229480455e031c17d29f3c6
vetonf <vetonfetai1@gmail.com>
2024-11-22 13:38:43 +0100

    first changes

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Front from "./components/front";
+
+function App() {
+  return (
+    <Front/>
+  );
+}
+
+export default App;
---
+++ src/components/front.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './front.css';
+
+const Front = () => {
+    return (
+      <nav className="navbar"><p className="nav-content">Home</p></nav>
+
+    );
+};
+
+export default Front;
